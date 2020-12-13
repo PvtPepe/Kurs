cc5da130eb28faf654b3f82ad2787487cf79acc3
PvtPepe <dgrib113@gmail.com>
2020-12-13 11:33:13 +0300

    3

---
+++ ClinicModule/ClinicModuleModule.cs
@@ -0,0 +1,20 @@
+﻿using ClinicModule.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace ClinicModule
+{
+    public class ClinicModuleModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
